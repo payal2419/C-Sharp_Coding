52d4535d8c51848725af70ddf16fb075d9810361
payal2419 <payal.s.pawar@gmail.com>
2021-09-07 16:48:54 +0530

    Payal

---
+++ Demo1/String.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class String
+    {
+        public static void Main(string[] args)
+        {
+            string n;
+            Console.WriteLine("Enter your name: ");
+            n = Console.ReadLine();
+            Console.WriteLine("Hello ! " + n);
+            Console.ReadLine();
+        }
+    }
+}
