e3aa6ad1b3173e036644289ae5af4baffa42582c
payal2419 <payal.s.pawar@gmail.com>
2021-09-07 20:24:02 +0530

    Payal

---
+++ Demo1/Even_Odd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Even_Odd
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter the number(n): ");
+            n = Convert.ToInt32 (Console.ReadLine());
+            if(n%2==0)
+            {
+                Console.WriteLine("It is a Even Number");
+            }
+            else  
+            {
+                Console.WriteLine("It is a Odd Number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
