14ca7bdc12b4a598cc02081b3c0ce21ec29402ab
payal2419 <payal.s.pawar@gmail.com>
2021-09-14 21:19:56 +0530

    Payal

---
+++ Demo1/GreatestNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Demo1
+{
+    class GreatestNumber
+    {
+        public static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter the value for a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for b");
+            b = Convert.ToInt32(Console.ReadLine());
+            if(a>b)
+            {
+                Console.WriteLine("It is the greatest number");
+            }
+            else
+            {
+                Console.WriteLine("It is not the greatest number");
+            }
+        }
+    }
+}
