d6178ce99d06b953724565ef81fae121f9f71ba4
payal2419 <payal.s.pawar@gmail.com>
2021-09-16 21:09:23 +0530

    Payal

---
+++ Demo1/TrafficSignal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Demo1
+{
+    class TrafficSignal
+    {
+        public static void Main(string[] args)
+        {
+            string Light;
+            Console.WriteLine("Enter the traffic light color");
+            Light = Console.ReadLine();
+            if(Light=="red" || Light=="Red" || Light=="RED")
+            {
+                Console.WriteLine("Hey, Hello Stop Right Now");
+            }
+            else if(Light=="green" || Light=="Green" || Light=="GREEN")
+            {
+                Console.WriteLine("Hey, You Can Gear Up to Gooooooo");
+            }
+            else if(Light=="yellow" || Light=="Yellow" || Light=="YELLOW")
+            {
+                Console.WriteLine("Hey, Slow Down Yellow Signal Here");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Color");
+            }
+            Console.ReadLine();
+        }
+    }
+}
