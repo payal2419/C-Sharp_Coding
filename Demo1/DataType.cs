fc8a59b52fed4a2e6a38b6d7c03b5f72553c9307
payal2419 <payal.s.pawar@gmail.com>
2021-12-05 22:00:55 +0530

    Payal

---
+++ Demo1/DataType.cs
@@ -0,0 +1,24 @@
+﻿
+//Add the correct data for the following variables
+using System;
+
+namespace Demo1
+{
+    class DataType
+    {
+        public static void Main(string[] args)
+        {
+            int myNum = 9;
+            double myDoubleNum = 8.99;
+            char myLetter = 'A';
+            bool myBoolean = false;
+            string myText = "Hello World";
+            Console.WriteLine("This is a Int DataType number i.e. " +myNum);
+            Console.WriteLine("This is a Double DataType i.e. " +myDoubleNum);
+            Console.WriteLine("This is a Character DataType i.e. " +myLetter);
+            Console.WriteLine("This is a Boolean DataType Number i.e. " +myBoolean);
+            Console.WriteLine("This is a String DataType i.e. " +myText);
+            Console.ReadLine();
+        }
+    }
+}
