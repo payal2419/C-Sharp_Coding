79f1d691f014c685f0b407850c0cb95064dc37a9
payal2419 <payal.s.pawar@gmail.com>
2021-12-05 22:22:11 +0530

    Payal

---
+++ Demo1/BooleanDataType.cs
@@ -0,0 +1,18 @@
+﻿
+//Create two boolean variables, named yay and nay, and add appropriate values to them
+using System;
+
+namespace Demo1
+{
+    class BooleanDataType
+    {
+        public static void Main(string[] args)
+        {
+            bool yay = true;
+            bool nay = false;
+            Console.WriteLine("Yay means Yes " + yay);
+            Console.WriteLine("Nay means Yes " + nay);
+            Console.ReadLine();
+        }
+    }
+}
