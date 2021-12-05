751e9ce96f3ace47725e157840591ad9f9ce0f8a
payal2419 <payal.s.pawar@gmail.com>
2021-12-05 20:36:41 +0530

    Payal

---
+++ Demo1/SumXY.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class SumXY
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(x + y);
+            Console.ReadLine();
+        }
+    }
+}
