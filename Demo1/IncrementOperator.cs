2d30a9a27bbc84e01ccff87402fa849835b880b4
payal2419 <payal.s.pawar@gmail.com>
2021-12-06 22:47:32 +0530

    Payal

--- Demo1/IncrementOperator.cs
+++ Demo1/IncrementOperator.cs
@@ -1,11 +1,16 @@
 ﻿
-//
+//Use the correct operator to increase the value of the variable x by 1
 using System;
 
 namespace Demo1
 {
     class IncrementOperator
     {
-
+        public static void Main(string[] args)
+        {
+            int x = 10;
+            x++;
+            Console.WriteLine("The incremented number is : " + x++);
+        }
     }
 }
