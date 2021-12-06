f469a75417cb3a9bac6c57cb0255e550ebd25ffe
payal2419 <payal.s.pawar@gmail.com>
2021-12-06 22:36:49 +0530

    Payal

---
+++ Demo1/Multiply.cs
@@ -0,0 +1,17 @@
+﻿
+//Create a program to multiply & Divide 10 with 5 and print the result
+using System;
+
+namespace Demo1
+{
+    class Multiply
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(10 * 5);
+            Console.WriteLine(10 / 5);
+
+        }
+        
+    }
+}
