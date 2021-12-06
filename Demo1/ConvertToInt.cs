ec8e80b3218844fe5c401ec24e586ef4913dc758
payal2419 <payal.s.pawar@gmail.com>
2021-12-06 20:32:03 +0530

    Payal

---
+++ Demo1/ConvertToInt.cs
@@ -0,0 +1,17 @@
+﻿
+//Create a program to print a number put in by the user
+using System;
+
+namespace Demo1
+{
+    class ConvertToInt
+    {
+        public static void Main(string[] args)
+        {
+            int myNum;
+            Console.WriteLine("Think of a number: ");
+            myNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your number is: " + myNum);
+        }
+    }
+}
---
+++ Demo1/UserInput.cs
@@ -0,0 +1,16 @@
+﻿
+//Create a program to get user input, stored in the variable userName
+using System;
+
+namespace Demo1
+{
+    class UserInput
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the UserName: ");
+            string userName = Console.ReadLine(); //to take input from user
+            Console.WriteLine("The UserName is: " + userName);
+        }
+    }
+}
